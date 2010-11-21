e6f94731b7884109763489e2217ad5cb284eec1a
Christoph <christoph@webvariants.de>
2010-11-21 19:45:55 +0100

    added license headers

--- tests/Library.php
+++ tests/Library.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Copyright (c) 2010, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
 
 function success($text) {
 	print '<span style="color:green"><strong>SUCCESS:</strong> '.$text.'</span>';
--- tests/index.php
+++ tests/index.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Copyright (c) 2010, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
 
 error_reporting(E_ALL | E_STRICT | E_NOTICE);
 ini_set('display_errors', 'On');
