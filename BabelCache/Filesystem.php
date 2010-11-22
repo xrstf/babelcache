a6d58479789db3f51358e68c11cbfb69528dcdc2
Christoph <christoph@webvariants.de>
2010-11-22 15:30:12 +0100

    documented filesystem cache

--- BabelCache/Filesystem.php
+++ BabelCache/Filesystem.php
@@ -8,12 +8,48 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
+/**
+ * Filesystem Cache
+ *
+ * This is the fallback caching strategy, that should be used if no in-memory
+ * cache is available. It only requires write permissions to a specific
+ * directory.
+ *
+ * Namespaces will be mapped to directories and sub-directories. Each namespace
+ * folder will then have a data~ directory in which the cache files will be
+ * placed. Because of the restriction in BabelCache::checkString(), you cannot
+ * use 'data~' in your namespaces, so no collisions can occur.
+ *
+ * Even though most modern filesystems can easily handle thousands of files in
+ * a single directory, this cache will automatically split your data up into
+ * smaller directories, based on the calculated hash. So if the key 'myvalue'
+ * is hashed '3423g4234...', the file will be placed in '/data~/34/3423g4234'.
+ *
+ * By changing cutHash(), you can adjust the length of those splitter
+ * directories (1 if you only have a few values, 10 if you expected gazillions
+ * of values). You can't change this value at runtime since it would totally
+ * mess up your cache directories.
+ *
+ * For faster access time, all read values will be stored in an internal memory
+ * cache.
+ *
+ * @author Christoph Mewes
+ * @see    BabelCache_Memory
+ */
 class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
-	protected $dataDir    = '';
-	protected $quickCache = null;
+	protected $dataDir    = '';    ///< string                absolute path to the cache directory
+	protected $quickCache = null;  ///< BabelCache_Interface  runtime cache for faster access times
 
-	private static $safeDirChar = '~';
+	private static $safeDirChar = '~'; ///< string  special character that is used for the data directory
 
+	/**
+	 * Constructor
+	 *
+	 * Creates the object, tries to create the data directory and creates a new
+	 * memory cache.
+	 *
+	 * @param string $dataDirectory  the full path to the cache directory (chmod 777)
+	 */
 	public function __construct($dataDirectory) {
 		clearstatcache();
 		self::makeDir($dataDirectory);
@@ -22,9 +58,6 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		$this->quickCache = new BabelCache_Memory();
 	}
 
-	/**
-	 * @return boolean  always true
-	 */
 	public static function isAvailable() {
 		return true;
 	}
@@ -70,7 +103,7 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 	public function set($namespace, $key, $value) {
 		$this->checkString($namespace);
 
-		$this->quickCache->set(self::getMemNamespace($namespace), $key, $value);
+		$this->quickCache->set($namespace, $key, $value);
 
 		$filename = $this->getFilename($namespace, $key);
 		$level    = error_reporting(0);
@@ -85,23 +118,21 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 	public function get($namespace, $key, $default = null) {
 		$this->checkString($namespace);
 
-		$memNamespace = self::getMemNamespace($namespace);
-
-		if ($this->quickCache->exists($memNamespace, $key)) {
-			return $this->quickCache->get($memNamespace, $key);
+		if ($this->quickCache->exists($namespace, $key)) {
+			return $this->quickCache->get($namespace, $key);
 		}
 
 		$data = @file_get_contents($this->getFilename($namespace, $key));
 		$data = $data === false ? $default : unserialize($data);
 
-		$this->quickCache->set($memNamespace, $key, $data);
+		$this->quickCache->set($namespace, $key, $data);
 		return $data;
 	}
 
 	public function exists($namespace, $key) {
 		$this->checkString($namespace);
 
-		if ($this->quickCache->exists(self::getMemNamespace($namespace), $key)) {
+		if ($this->quickCache->exists($namespace, $key)) {
 			return true;
 		}
 
@@ -111,7 +142,7 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 	public function delete($namespace, $key) {
 		$this->checkString($namespace);
 
-		$this->quickCache->delete(self::getMemNamespace($namespace), $key);
+		$this->quickCache->delete($namespace, $key);
 		clearstatcache();
 
 		return @unlink($this->getFilename($namespace, $key));
@@ -122,7 +153,7 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 
 		// flush quick cache
 
-		$this->quickCache->flush(self::getMemNamespace($namespace), $recursive);
+		$this->quickCache->flush($namespace, $recursive);
 
 		// handle our own cache
 
@@ -147,10 +178,20 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		}
 	}
 
-	private static function getMemNamespace($namespace) {
-		return 'fscache.'.$namespace;
-	}
-
+	/**
+	 * Creates the directories for a given namespace
+	 *
+	 * This method will recursively create the required directories to map a
+	 * given namespace. Every created directory will be CHMOD 777.
+	 *
+	 * In the last step, a 'data~' directory is created, which will contain the
+	 * partial hash directory (first two characters of the elements hash).
+	 *
+	 * @param  array  $namespace  the namespace, already split up
+	 * @param  string $root       the current root directory (used for recursion)
+	 * @param  string $hash       the hash of the element that will be stored
+	 * @return boolean            always true
+	 */
 	private static function createNamespaceDir($namespace, $root, $hash) {
 		if (!empty($namespace)) {
 			$thisPart = array_shift($namespace);
@@ -180,22 +221,38 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		}
 	}
 
-	private static function dataDirExists($namespace, $root) {
+	/**
+	 * Checks if the target directory exists
+	 *
+	 * @param  string $namespace  the target namespace
+	 * @return boolean            true if the directory exists, else false
+	 */
+	private static function dataDirExists($namespace) {
 		$namespace = self::getDirFromNamespace($namespace);
 		$dataDir   = 'data'.self::$safeDirChar;
-		$dirname   = $root.'/'.$namespace.'/'.$dataDir;
+		$dirname   = $this->dataDir.'/'.$namespace.'/'.$dataDir;
 
 		clearstatcache();
 		return is_dir($dirname);
 	}
 
+	/**
+	 * Gets the full filename of a cache element
+	 *
+	 * This method computes the absolute path for a given cache element. Missing
+	 * directories will be created automatically.
+	 *
+	 * @param  string $namespace  element namespace
+	 * @param  string $key        element key
+	 * @return string             the full path
+	 */
 	private function getFilename($namespace, $key) {
 		$namespace = $this->checkString($namespace);
 		$key       = $this->checkString($key);
 		$dir       = $this->dataDir;
 		$hash      = md5($key);
 
-		if (!self::dataDirExists($namespace, $dir)) {
+		if (!self::dataDirExists($namespace)) {
 			self::createNamespaceDir(explode('.', $namespace), $dir, $hash);
 		}
 
@@ -210,6 +267,15 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		return $dir.'/'.$hash;
 	}
 
+	/**
+	 * Get a list of all known sub-namespaces
+	 *
+	 * This will scan a namesace directory for every child directory (except
+	 * 'data~' of course) and return the basenames of all directories (sorted).
+	 *
+	 * @param  string $namespace  the target namespace
+	 * @return array              a sorted list ob sub-directories
+	 */
 	private static function getSubNamespaces($namespace) {
 		$namespace = self::getDirFromNamespace($this->checkString($namespace));
 		$dir       = $this->dataDir.'/'.$namespace;
@@ -231,6 +297,15 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		return array_values($namespaces);
 	}
 
+	/**
+	 * Removes a directory and all children
+	 *
+	 * This method is used for flushing namespaces and removes all files and
+	 * directories (recursively) in a given root directory.
+	 *
+	 * @param  string $root  the root from where to start
+	 * @return boolean       true if everything was deleted, else false
+	 */
 	private static function deleteRecursive($root) {
 		if (!is_dir($root)) {
 			return true;
@@ -261,18 +336,42 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 	}
 
 	/**
-	 * @param string $namespace
+	 * Makes a directory fragment out of a namespace
+	 *
+	 * This is a simple helper that will replace all dots with slashes.
+	 *
+	 * @param  string $namespace  the namespace
+	 * @return string             the namespace with replaced dots
 	 */
 	private static function getDirFromNamespace($namespace) {
 		return str_replace('.', '/', $namespace);
 	}
 
+	/**
+	 * Creates a directory
+	 *
+	 * This method will try to create a directory and chmod 777 it. If this
+	 * fails, an exception is thrown.
+	 *
+	 * @throws BabelCache_Exception  if the directory could not be created
+	 * @param  string $dir           the directory to create
+	 */
 	private static function makeDir($dir) {
 		if (!is_dir($dir) && !@mkdir($dir, 0777, true)) {
 			throw new BabelCache_Exception('Can\'t create directory in '.$dir.'.');
 		}
 	}
 
+	/**
+	 * Cut out part of a hash
+	 *
+	 * This method will return the first 2 characters of a given element hash.
+	 * The partial hash is used to evenly distribute the cache elements in a
+	 * single namespace.
+	 *
+	 * @param  string $hash  the element's hash
+	 * @return string        the first 2 characters of that hash
+	 */
 	private static function cutHash($hash) {
 		return substr($hash, 0, 2);
 	}
