9b3ef14849791a53ed364beb7fd11a7f0f640eb5
Christoph <christoph@webvariants.de>
2011-08-10 00:16:09 +0200

    updated license for 2011 (about time, eh?)

--- tests/index.php
+++ tests/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) 2010, webvariants GbR, http://www.webvariants.de
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
  *
  * This file is released under the terms of the MIT license. You can find the
  * complete text in the attached LICENSE file or online at:
