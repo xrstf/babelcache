7664fc2e040bd203b90c993c57fb1fdea979ea99
Christoph <christoph@webvariants.de>
2013-05-19 03:54:11 +0200

    added draft of the future PSR support
    --HG--
    branch : next

---
+++ src/wv/BabelCache/Psr/Cache.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace wv\BabelCache\Psr;
+
+use Psr\Cache\CacheInterface;
+use Psr\Cache\CacheItemInterface;
+use wv\BabelCache\Adapter\AdapterInterface;
+
+/**
+ * Wrapper around BabelCache's interface to be PSR compliant
+ *
+ * Use this if you really need to and can afford the overhead of wrapping all
+ * your data in CacheItem instances.
+ *
+ * By design, this implementation does not support any kind of namespacing.
+ *
+ * @author  Christoph Mewes
+ * @package BabelCache
+ */
+class Cache implements CacheInterface {
+	protected $adapter;
+
+	public function __construct(AdapterInterface $adapter) {
+		$this->adapter = $adapter;
+	}
+
+	public function get($key) {
+		$found = false;
+		$value = $this->adapter->get($key, $found);
+
+		return new CacheItem($key, $found ? $value : null, $found);
+	}
+
+	public function set($key, $value, $ttl = null) {
+		$this->adapter->set($key, $value, $ttl);
+
+		return true;
+	}
+
+	public function remove($key) {
+		$this->adapter->delete($key, $value, $ttl);
+
+		return true;
+	}
+
+	public function getMultiple($keys) {
+		$result = array();
+
+		// extremely simple solution, this can be improved by extending the adapter interfaces sometime
+		foreach ($keys as $key) {
+			$result[$key] = $this->get($key);
+		}
+
+		return $result;
+	}
+
+	public function setMultiple($items, $ttl = null) {
+		foreach ($items as $item) {
+			$this->set($item->getKey(), $item->getValue(), $ttl);
+		}
+
+		return true;
+	}
+
+	public function removeMultiple($keys) {
+		$result = array();
+
+		// extremely simple solution, this can be improved by extending the adapter interfaces sometime
+		foreach ($keys as $key) {
+			$result[$key] = $this->remove($key);
+		}
+
+		return $result;
+	}
+
+	public function clear() {
+		$this->adapter->clear();
+
+		return true;
+	}
+}
---
+++ src/wv/BabelCache/Psr/CacheItem.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace wv\BabelCache\Psr;
+
+use Psr\Cache\CacheItemInterface;
+
+/**
+ * Basic implementation for PSR's cache item
+ *
+ * @author  Christoph Mewes
+ * @package BabelCache
+ */
+class CacheItem implements CacheItemInterface {
+	protected $key;
+	protected $value;
+	protected $isHit;
+
+	public function __construct($key, $value, $isHit) {
+		$this->key   = $key;
+		$this->value = $value;
+		$this->isHit = !!$isHit;
+	}
+
+	public function getKey() {
+		return $this->key;
+	}
+
+	public function getValue() {
+		return $this->value;
+	}
+
+	public function setValue($value) {
+		$this->value = $value;
+	}
+
+	public function isHit() {
+		return $this->isHit;
+	}
+}
