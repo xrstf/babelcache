f53860d91407afcc6556f731bc90d2a047d87f49
Christoph <christoph@webvariants.de>
2013-05-22 03:06:15 +0200

    transformed the filesystem cache into a simple key-value adapter
    --HG--
    branch : next

--- src/wv/BabelCache/Adapter/Filesystem.php
+++ src/wv/BabelCache/Adapter/Filesystem.php
@@ -8,114 +8,72 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
+namespace wv\BabelCache\Adapter;
+
+use wv\BabelCache\AdapterInterface;
+use wv\BabelCache\Exception;
+use wv\BabelCache\LockingInterface;
+
 /**
  * Filesystem Cache
  *
- * Namespaces will be mapped to directories and sub-directories. Each namespace
- * folder will then have a data~ directory in which the cache files will be
- * placed. Because of the restriction in BabelCache::checkString(), you cannot
- * use 'data~' in your namespaces, so no collisions can occur.
- *
- * Even though most modern filesystems can easily handle thousands of files in
- * a single directory, this cache will automatically distribute your data in
- * smaller directories, based on the calculated hash. So if the key 'myvalue'
- * is hashed '3423g4234...', the file will be placed in '/data~/34/3423g4234'.
- *
- * By changing cutHash(), you can adjust the length of those splitter
- * directories (1 if you only have a few values, 10 if you expected gazillions
- * of values). You can't change this value at runtime since it would totally
- * mess up your cache directories. Returning an empty string disables the
- * distribution.
- *
- * In most cases, this strategy to spread the data files is not necessary.
- * Before using it, consider using %BabelCache_Filesystem_Plain instead, which
- * should suffice in 99,9% of all cases (if you really need to store large
- * amount of data, consider using Memcached instead).
- *
- * @see     BabelCache_Filesystem_Plain
  * @package BabelCache.Adapter
  */
-class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
-	protected $dataDir = '';    ///< string  absolute path to the cache directory
-
-	protected static $safeDirChar = '~';  ///< string  special character that is used for the data directory
-	protected static $dirPerm     = 0777; ///< int     permissions to use for created directories
-	protected static $filePerm    = 0664; ///< int     permissions to use for created files
+class Filesystem implements AdapterInterface, LockingInterface {
+	protected $dataDir;   ///< string  absolute path to the cache directory
+	protected $filePerm;  ///< int     permissions to use for created files
+	protected $dirPerm;   ///< int     permissions to use for lock directories
 
 	/**
 	 * Constructor
 	 *
-	 * Creates the object, tries to create the data directory and creates a new
-	 * memory cache.
-	 *
-	 * @param string $dataDirectory  the full path to the cache directory (chmod 777)
+	 * @param string $dataDirectory  the full path to the cache directory
+	 * @param int    $filePerm       file permissions to use
+	 * @param int    $dirPerm        directory permissions to use
 	 */
-	public function __construct($dataDirectory) {
-		$this->makeDir($dataDirectory);
-		$this->dataDir = $dataDirectory;
+	public function __construct($dataDirectory, $filePerm = 0664, $dirPerm = 0777) {
+		if (!is_dir($dataDirectory)) {
+			throw new Exception('Invalid cache directory "'.$dataDirectory.'" given.');
+		}
+
+		$this->dataDir  = $dataDirectory;
+		$this->filePerm = (int) $filePerm;
+		$this->dirPerm  = (int) $dirPerm;
 	}
 
+	/**
+	 * Checks whether a caching system is avilable
+	 *
+	 * This method will be called before an instance is created. It is supposed
+	 * to check for the required functions and whether user data caching is
+	 * enabled.
+	 *
+	 * @return boolean  true if the cache can be used, else false
+	 */
 	public static function isAvailable() {
 		return true;
 	}
 
-	public function lock($namespace, $key, $duration = 1) {
-		$key = $this->getFullKeyHelper($namespace, $key);
-		$dir = $this->dataDir.'/lock#'.$key;
-
-		clearstatcache();
-		return @mkdir($dir, self::$dirPerm);
-	}
-
-	public function unlock($namespace, $key) {
-		$key = $this->getFullKeyHelper($namespace, $key);
-		$dir = $this->dataDir.'/lock#'.$key;
-
-		clearstatcache();
-		return is_dir($dir) ? rmdir($dir) : true;
-	}
-
-	public function waitForObject($namespace, $key, $default = null, $maxWaitTime = 3, $checkInterval = 50) {
-		$key            = $this->getFullKeyHelper($namespace, $key);
-		$dir            = $this->dataDir.'/lock#'.$key;
-		$start          = microtime(true);
-		$waited         = 0;
-		$checkInterval *= 1000;
-
-		while ($waited < $maxWaitTime && is_dir($dir)) {
-			usleep($checkInterval);
-			$waited = microtime(true) - $start;
-			clearstatcache();
-		}
-
-		if (!is_dir($dir)) {
-			return $this->get($namespace, $key, $default);
-		}
-
-		return $default;
-	}
-
-	public function set($namespace, $key, $value) {
-		$filename = $this->getFilename($namespace, $key);
-		$level    = error_reporting(0);
-
-		touch($filename, self::$filePerm);
-		file_put_contents($filename, serialize($value), LOCK_EX);
-
-		error_reporting($level);
-		return $value;
-	}
-
-	public function get($namespace, $key, $default = null, &$found = null) {
+	/**
+	 * Gets a value out of the cache
+	 *
+	 * This method will try to read the value from the cache. If it's not found,
+	 * $default will be returned.
+	 *
+	 * @param  string  $key    the object key
+	 * @param  boolean $found  will be set to true or false when the method is finished
+	 * @return mixed           the found value or null
+	 */
+	public function get($key, $default = null, &$found = null) {
 		$found = false;
-		$file  = $this->getFilename($namespace, $key);
+		$file  = $this->getFilename($key);
 
 		if (!file_exists($file)) {
 			return $default;
 		}
 
 		// open the file
-		$handle = @fopen($file, 'r');
+		$handle = @fopen($file, 'rb');
 
 		// old filestats?
 		if (!$handle) {
@@ -135,239 +93,125 @@ class BabelCache_Filesystem extends BabelCache implements BabelCache_Interface {
 		// unlock it again
 		flock($handle, LOCK_UN);
 		fclose($handle);
+
 		$found = true;
+
 		return unserialize($data);
 	}
 
-	public function exists($namespace, $key) {
-		return file_exists($this->getFilename($namespace, $key));
-	}
+	/**
+	 * Sets a value
+	 *
+	 * This method will put a value into the cache. If it already exists, it
+	 * will be overwritten.
+	 *
+	 * @param  string $key    the object key
+	 * @param  mixed  $value  the value to store
+	 * @return mixed          the set value
+	 */
+	public function set($key, $value) {
+		$filename = $this->getFilename($key);
+		$level    = error_reporting(0);
 
-	public function delete($namespace, $key) {
-		return @unlink($this->getFilename($namespace, $key));
-	}
+		touch($filename, $this->filePerm);
+		file_put_contents($filename, serialize($value), LOCK_EX);
 
-	public function flush($namespace, $recursive = false) {
-		$this->checkString($namespace, 'namespace');
+		error_reporting($level);
 
-		// handle our own cache
-
-		$namespace = $this->getDirFromNamespace($namespace);
-		$root      = $this->dataDir.'/'.$namespace;
-
-		// Wenn wir rekursiv löschen, können wir wirklich alles in diesem Verzeichnis
-		// löschen.
-
-		if ($recursive) {
-			return $this->deleteRecursive($root);
-		}
-
-		// Löschen wir nicht rekursiv, dürfen wir nur das data~-Verzeichnis
-		// entfernen.
-
-		else {
-			$dataDir = 'data'.self::$safeDirChar;
-			return $this->deleteRecursive($root.'/'.$dataDir);
-		}
+		return $value;
 	}
 
 	/**
-	 * Creates the directories for a given namespace
+	 * Checks whether a value exists
 	 *
-	 * This method will recursively create the required directories to map a
-	 * given namespace. Every created directory will be CHMOD 777.
-	 *
-	 * In the last step, a 'data~' directory is created, which will contain the
-	 * partial hash directory (first two characters of the elements hash).
-	 *
-	 * @param  array  $namespace  the namespace, already split up
-	 * @param  string $root       the current root directory (used for recursion)
-	 * @param  string $hash       the hash of the element that will be stored
-	 * @return boolean            always true
+	 * @param  string $key  the object key
+	 * @return boolean      true if the value exists, else false
 	 */
-	protected function createNamespaceDir($namespace, $root, $hash) {
-		if (!empty($namespace)) {
-			$thisPart = array_shift($namespace);
-			$dir      = $root.'/'.$thisPart;
-
-			$this->makeDir($dir);
-			return $this->createNamespaceDir($namespace, $dir, $hash);
-		}
-		else {
-			// Zuletzt erzeugen wir das Verteilungsverzeichnis für die Cache-Daten,
-			// damit nicht alle Dateien in einem großen Verzeichnis leben müssen.
-
-			// Dazu legen wir in dem Zielnamespace ein Verzeichnis "data~" an,
-			// in dem die 00, 01, ... 99 ... FE, FF-Verzeichnisse erzeugt werden.
-			// Dadurch vermeiden wir Kollisionen mit Namespaces, die auch Teilnamespaces
-			// der Länge 2 haben.
-
-			$dir = $root.'/data'.self::$safeDirChar;
-			$this->makeDir($dir);
-
-			// Jetzt kommen die kleinen Verzeichnisse...
-
-			$hash = $this->cutHash($hash);
-
-			if (strlen($hash) > 0) {
-				$dir = $dir.'/'.$hash;
-				$this->makeDir($dir);
-			}
-
-			return true;
-		}
+	public function exists($key) {
+		return is_file($this->getFilename($key));
 	}
 
 	/**
-	 * Checks if the target directory exists
+	 * Removes a single value from the cache
 	 *
-	 * @param  string $namespace  the target namespace
-	 * @param  string $root       the root cach directory
-	 * @return boolean            true if the directory exists, else false
+	 * @param  string $key  the object key
+	 * @return boolean      true if the value was deleted, else false
 	 */
-	protected function dataDirExists($namespace, $root) {
-		$namespace = $this->getDirFromNamespace($namespace);
-		$dataDir   = 'data'.self::$safeDirChar;
-		$dirname   = $root.'/'.$namespace.'/'.$dataDir;
-
-		clearstatcache();
-		return is_dir($dirname);
+	public function remove($key) {
+		return @unlink($this->getFilename($key));
 	}
 
 	/**
-	 * Gets the full filename of a cache element
+	 * Removes all values
 	 *
-	 * This method computes the absolute path for a given cache element. Missing
-	 * directories will be created automatically.
-	 *
-	 * @param  string $namespace  element namespace
-	 * @param  string $key        element key
-	 * @return string             the full path
+	 * @return boolean  true if the flush was successful, else false
 	 */
-	protected function getFilename($namespace, $key) {
-		$namespace = $this->checkString($namespace, 'namespace');
-		$key       = $this->checkString($key, 'key');
-		$dir       = $this->dataDir;
-		$hash      = md5($key);
-
-		if (!$this->dataDirExists($namespace, $dir)) {
-			$this->createNamespaceDir(explode('.', $namespace), $dir, $hash);
-		}
-
-		// Finalen Dateipfad erstellen
-
-		$namespace = $this->getDirFromNamespace($namespace);
-		$dataDir   = 'data'.self::$safeDirChar;
-		$hashPart  = $this->cutHash($hash);
-		$dir       = $dir.'/'.$namespace.'/'.$dataDir;
-
-		if (strlen($hashPart) > 0) {
-			$dir .= '/'.$hashPart;
-			$this->makeDir($dir);
-		}
-
-		return $dir.'/'.$hash;
-	}
-
-	/**
-	 * Removes a directory and all children
-	 *
-	 * This method is used for flushing namespaces and removes all files and
-	 * directories (recursively) in a given root directory.
-	 *
-	 * @param  string $root  the root from where to start
-	 * @return boolean       true if everything was deleted, else false
-	 */
-	protected function deleteRecursive($root) {
-		if (!is_dir($root)) {
-			return true;
-		}
-
+	public function clear() {
 		try {
-			$dirIterator = new RecursiveDirectoryIterator($root);
-			$recIterator = new RecursiveIteratorIterator($dirIterator, RecursiveIteratorIterator::CHILD_FIRST);
-			$status      = true;
-			$level       = error_reporting(0);
+			$status   = true;
+			$level    = error_reporting(0);
+			$iterator = new \DirectoryIterator($this->dataDir);
 
-			foreach ($recIterator as $file) {
-				if ($file->isDir()) $status &= rmdir($file);
-				elseif ($file->isFile()) $status &= unlink($file);
+			foreach ($iterator as $file) {
+				if ($file->isFile()) {
+					$status &= unlink($file->getPathname());
+				}
+
+				// try to remove locks as well
+				elseif ($file->isDir() && !$file->isDot()) {
+					$status &= rmdir($file->getPathname());
+				}
 			}
-
-			rmdir($root);
-
-			$recIterator = null;
-			$dirIterator = null;
 
 			clearstatcache();
 			error_reporting($level);
-			return $status;
+
+			return !!$status;
 		}
-		catch (UnexpectedValueException $e) {
+		catch (\Exception $e) {
 			return false;
 		}
 	}
 
 	/**
-	 * Makes a directory fragment out of a namespace
+	 * Locks a key
 	 *
-	 * This is a simple helper that will replace all dots with slashes.
+	 * This method will create a lock for a specific key.
 	 *
-	 * @param  string $namespace  the namespace
-	 * @return string             the namespace with replaced dots
+	 * @param  string $key  the key
+	 * @return boolean      true if the lock was aquired, else false
 	 */
-	protected function getDirFromNamespace($namespace) {
-		return str_replace('.', '/', $namespace);
+	public function lock($key) {
+		$dir = $this->dataDir.'/lock-'.sha1($key);
+
+		clearstatcache();
+
+		return @mkdir($dir, $this->dirPerm);
 	}
 
 	/**
-	 * Creates a directory
+	 * Releases a lock
 	 *
-	 * This method will try to create a directory and chmod 777 it. If this
-	 * fails, an exception is thrown.
+	 * This method will remove a lock for a specific key.
 	 *
-	 * @throws BabelCache_Exception  if the directory could not be created
-	 * @param  string $dir           the directory to create
+	 * @param  string $key  the key
+	 * @return boolean      true if the lock was released or there was no lock, else false
 	 */
-	protected function makeDir($dir) {
-		if (!is_dir($dir)) {
-			if (!@mkdir($dir, self::$dirPerm, true)) {
-				throw new BabelCache_Exception('Can\'t create directory in '.$dir.'.');
-			}
+	public function unlock($key) {
+		$dir = $this->dataDir.'/lock-'.sha1($key);
 
-			clearstatcache();
-		}
+		clearstatcache();
+
+		return is_dir($dir) ? rmdir($dir) : true;
 	}
 
 	/**
-	 * Cut out part of a hash
+	 * Gets the full filename of a cache element
 	 *
-	 * This method will return the first 2 characters of a given element hash.
-	 * The partial hash is used to evenly distribute the cache elements in a
-	 * single namespace.
-	 *
-	 * @param  string $hash  the element's hash
-	 * @return string        the first 2 characters of that hash
+	 * @param  string $key  element key
+	 * @return string       the full path
 	 */
-	protected function cutHash($hash) {
-		return substr($hash, 0, 2);
-	}
-
-	/**
-	 * Set the permissions
-	 *
-	 * @param int $chmod  the new chmod value (like 0777)
-	 */
-	public static function setDirPermissions($chmod) {
-		self::$dirPerm = (int) $chmod;
-	}
-
-	/**
-	 * Set the permissions
-	 *
-	 * @param int $chmod  the new chmod value (like 0777)
-	 */
-	public static function setFilePermissions($chmod) {
-		self::$filePerm = (int) $chmod;
+	protected function getFilename($key) {
+		return $this->dataDir.'/'.sha1($key);
 	}
 }
